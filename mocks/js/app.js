c9f0d7f8c3ac9b73335e0fa65c4841ff0442a05f
Mike Newell <mike@iwearshorts.com>
2012-12-03 14:15:42 -0800

    added some common css styles to make on the box styleable

--- mocks/js/app.js
+++ mocks/js/app.js
@@ -3,12 +3,56 @@
  * and open the template in the editor.
  */
 
-var App = function() {
-    var init = function() {
+var App = Class.extend({
+    init: function() {
         
-    };
-    
-    init();
-};
+        var self = this;
+        
+        $('#submit input').on('click touchend', function(evt) {
+            evt.preventDefault();
+            var email = $('#email input').val();
+            var result = self.parseEmail(email);
+            
+            if(result) {
+                // TODO: make ajax call for next page
 
-var app = new App();
+                $('#wrap').animate({
+                    opacity: 0
+                }, 1000, function() {
+                    // complete
+                })
+            }
+                
+        });
+        
+    },
+    
+    /**
+     * Parse the user input to the email form field
+     * @access  public
+     * @param   string e a gspsf formatted email
+     * @return  array arr with full name, first name = arr[1], last name = arr[2]
+     *          or -1 if we don't have a valid email format
+     */
+    parseEmail: function() {
+        
+        if(e.search('_') === -1) {
+            return false;
+        }
+        
+        var regex = /([a-zA-Z]+)\_([a-zA-Z]+)/;
+
+        var fullName = regex.exec(email);
+
+        return fullName;
+        
+    }
+});
+
+(function($) {
+    $(window).load(function() {
+        var app = new App();
+    });
+})(jQuery);
+
+
