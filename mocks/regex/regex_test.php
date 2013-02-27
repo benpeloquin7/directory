653982a543dbc88e844fa20b1b45aea6ec9e7abd
Mike Newell <mike@iwearshorts.com>
2013-02-26 20:36:32 -0800

    added search algorithm thats fucking cool

--- mocks/regex/regex_test.php
+++ mocks/regex/regex_test.php
@@ -33,66 +33,137 @@ $terms = array(
     'E45',
     '7-141',
     '124A',
-    '123C'
+    '123C',
+    '653E mike_newell jeannine_caesar@gspdet.com rob canfield (303) 475-9613 creative account'
 );
 
 for($i = 0; $i < count($terms); $i++) {
     $matches = search_string($terms[$i]);
     echo '<br />';
-    echo 'Matches for string: <strong>' . $terms[$i] . '</strong> are: ' . $matches['types'];
+    echo 'Matches for string: <strong>' . $terms[$i] . '</strong> are: ';
     echo '<br /><pre>';
     print_r($matches);
-    echo '<br /></pre>';
+    echo '<br /><br /></pre>';
 }
 
 function search_string($phrase) {
     
     $term_arr = explode(' ', $phrase);
     $output = array();
-    $types = '';
+    $types = array();
+    $queries = array();
     
-    echo count($term_arr);
-    
-    if(count($term_arr) > 1) {
-        foreach($term_arr as $term) {
+    foreach($term_arr as $term) {
 
-            $phone_pattern = '#(([+]?[\(]?[0-9]{3}[\)]?)?.?([0-9]{3}.?[0-9]{4}))#';
-            preg_match($phone_pattern, $term, $phone_matches);
+        $phone_pattern = '#(([+]?[\(]?[0-9]{3}[\)]?)?.?([0-9]{3}.?[0-9]{4}))#';
+        preg_match($phone_pattern, $term, $phone_matches);
 
-            $seat_pattern = '#^[a-zA-Z0-9]?[-]?[0-9]{2,3}[a-zA-Z]?[^a-zA-Z0-9]?#';
-            preg_match($seat_pattern, $term, $seat_matches);
+        $seat_pattern = '#^[a-zA-Z0-9]?[-]?[0-9]{2,3}[a-zA-Z]?[^a-zA-Z0-9]?#';
+        preg_match($seat_pattern, $term, $seat_matches);
 
-            $username_pattern = '#^[a-zA-Z]+_[a-zA-Z]+$#';
-            preg_match($username_pattern, $term, $username_matches);
-
-            if($phone_matches) {
-                $output['phone'] = $phone_matches;
-                $types .= ' phone ';
-            } else if($seat_matches) {
-                $output['seat'] = $seat_matches;
-                $types .= ' seat ';
-            } else if($username_matches) {
-                $output['username'] = $username_matches;
-                $types .= ' username ';
-            } else if (filter_var($term, FILTER_VALIDATE_EMAIL)) {
-                $output['email'] =  $term;
-                $types .= ' email ';
-            } else {
-                $output[] = array('no');
-                $types .= ' multi-term '
-            }
-
-        }
-    } else {
-        $output['single-term'] = $term;
-        $types .= ' single-term ';
-    }
-    
+        $username_pattern = '#^[a-zA-Z]+_[a-zA-Z]+$#';
+        preg_match($username_pattern, $term, $username_matches);
         
-    
+        $word_patter = '#^[a-zA-Z]+$#';
+        preg_match($word_patter, $term, $word_matches);
+
+        if($phone_matches) {
+            $no_symbols_phone = preg_replace('#[^0-9]#i', '', $phone_matches[0]);
+            $no_symbols_phone = strval($no_symbols_phone);
+            if(strlen($no_symbols_phone) > 7) {
+                $formatted_phone = ''.substr($no_symbols_phone, 0, 3) . '-' . substr($no_symbols_phone, 3, 3) . '-' . substr($no_symbols_phone, 6, 4);
+            } else {
+                $formatted_phone = ''.substr($no_symbols_phone, 0, 3) . '-' . substr($no_symbols_phone, 3, 4);
+            }
+            $output['phone'][] = $formatted_phone;
+            $types[] = 'phone';
+            $queries[] = format_phone_query($formatted_phone);
+        } else if($seat_matches) {
+            $output['seat'] = $seat_matches;
+            $types[] = 'seat';
+            $queries[] = format_seat_query($seat_matches[0]);
+        } else if($username_matches) {
+            $output['username'] = $username_matches;
+            $types[] = 'username';
+            $queries[] = format_username_query($username_matches[0]);
+        } else if (filter_var($term, FILTER_VALIDATE_EMAIL)) {
+            $output['email'][] =  $term;
+            $types[] = 'email';
+            $queries[] = format_email_query($term);
+        } else if($word_matches) {
+            $output['word'][] = $term;
+            $types[] = 'word';
+            $queries[] = format_word_query($term);
+        } else {
+            $output[][] = $term;
+            $types[] = 'mixed';
+            $queries[] = format_mixed_query($term);
+        }
+
+    }
+        
     $output['types'] = $types;
+    $output['queries'] = $queries;
+    
+    $output['query'] = get_query($queries);
     
     return $output;
-};
+}
+
+function format_phone_query($formatted_phone_number) {
+    return '(ext LIKE \'%'.$formatted_phone_number.'%\') OR (mobile LIKE \'%'.$formatted_phone_number.'%\')';
+}
+
+function format_seat_query($seat) {
+    return '(seat LIKE \'%'.$seat.'%\')';
+}
+
+function format_username_query($username) {
+    return '(userName LIKE \'%'.$username.'%\')';
+}
+
+function format_email_query($email) {
+    return '(email LIKE \'%'.$email.'%\')';
+}
+
+function format_word_query($word) {
+    $output = '(firstName LIKE \'%'.$word.'%\') OR ';
+    $output .= '(lastName LIKE \'%'.$word.'%\') OR ';
+    $output .= '(title LIKE \'%'.$word.'%\') OR ';
+    $output .= '(dept LIKE \'%'.$word.'%\') OR ';
+    $output .= '(email LIKE \'%'.$word.'%\') OR ';
+    $output .= '(seat LIKE \'%'.$word.'%\')';
+
+    return $output;
+}
+
+function format_mixed_query($term) {
+    $output = '(firstName LIKE \'%'.$term.'%\') OR ';
+    $output .= '(lastName LIKE \'%'.$term.'%\') OR ';
+    $output .= '(title LIKE \'%'.$term.'%\') OR ';
+    $output .= '(dept LIKE \'%'.$term.'%\') OR ';
+    $output .= '(email LIKE \'%'.$term.'%\') OR ';
+    $output .= '(seat LIKE \'%'.$term.'%\')';
+
+    return $output;
+}
+
+function get_query($clauses) {
+    
+    $append = '';
+    
+    foreach($clauses as $key => $clause) {
+        if($key == 0) {
+            $append .= ' ' . $clause . ' ';
+        } else {
+            $append .= ' OR ' . $clause . ' ';
+        }
+        
+    }
+    
+    $query = 'SELECT * FROM people WHERE '.$append. ' LIMIT 0, 1000';
+    
+    return $query;
+}
 
 ?>
--- production/app/Controller/PeopleController.php
+++ production/app/Controller/PeopleController.php
@@ -118,69 +118,69 @@ class PeopleController extends AppController {
         // request must be post
         if($this->request->is('post')) {
             
-            $term = $this->request->data('Person.term');
+            $phrase = $this->request->data('Person.phrase');
             
-            if(!empty($term)) {
+            if(!empty($phrase)) {
                 
                 // we know its not empty - now determine the type
-                $type = determine_type($term);
+                $type_arr = determine_type($phrase);
                 
                 // set the catchall for the results
-                $results;
-                
-                // we know the type, now sanitize according to type
-                switch($term) {
-                    case 'phone':
-                        // below is an example of a search for phone in two fields
-                        $cleaned_term = sanitize_phone($term);
-                        
-                        $fields = array('ext', 'mobile');
-                        
-                        $results = $this->search_fields($fields, $cleaned_term);
-                        
-                        break;
-                    case 'email':
-                        $cleaned_term = filter_var($term, FILTER_SANITIZE_EMAIL);
-                        
-                        $fields = array('email');
-                        
-                        $results = $this->search_fields($fields, $cleaned_term);
-                        
-                        break;
-                    case 'multi-term':
-                        $cleaned_term = filter_var($term, FILTER_SANITIZE_STRING);
-                        
-                        $fields = array();
-                        
-                        $results = $this->search_fields($fields, $cleaned_term);
-                        
-                        
-                        break;
-                    case 'single-term':
-                        $cleaned_term = filter_var($term, FILTER_SANITIZE_STRING);
-                        
-                        $fields = array();
-                        
-                        $results = $this->search_fields($fields, $cleaned_term);
-                        
-                        break;
-                    case 'username':
-                        $cleaned_term = filter_var($term, FILTER_SANITIZE_STRING);
-                        
-                        $fields = array('userName');
-                        
-                        $results = $this->search_fields($fields, $cleaned_term);
-                        
-                        break;
-                    case 'seat':
-                        $cleaned_term = filter_var($term, FILTER_SANITIZE_NUMBER_INT);
-                        
-                        $fields = array('seat');
-                        
-                        $results = $this->search_fields($fields, $cleaned_term);
-                        
-                        break;
-                }
+//                $results;
+//                
+//                // we know the type, now sanitize according to type
+//                switch($term) {
+//                    case 'phone':
+//                        // below is an example of a search for phone in two fields
+//                        $cleaned_term = sanitize_phone($term);
+//                        
+//                        $fields = array('ext', 'mobile');
+//                        
+//                        $results = $this->search_fields($fields, $cleaned_term);
+//                        
+//                        break;
+//                    case 'email':
+//                        $cleaned_term = filter_var($term, FILTER_SANITIZE_EMAIL);
+//                        
+//                        $fields = array('email');
+//                        
+//                        $results = $this->search_fields($fields, $cleaned_term);
+//                        
+//                        break;
+//                    case 'multi-term':
+//                        $cleaned_term = filter_var($term, FILTER_SANITIZE_STRING);
+//                        
+//                        $fields = array();
+//                        
+//                        $results = $this->search_fields($fields, $cleaned_term);
+//                        
+//                        
+//                        break;
+//                    case 'single-term':
+//                        $cleaned_term = filter_var($term, FILTER_SANITIZE_STRING);
+//                        
+//                        $fields = array();
+//                        
+//                        $results = $this->search_fields($fields, $cleaned_term);
+//                        
+//                        break;
+//                    case 'username':
+//                        $cleaned_term = filter_var($term, FILTER_SANITIZE_STRING);
+//                        
+//                        $fields = array('userName');
+//                        
+//                        $results = $this->search_fields($fields, $cleaned_term);
+//                        
+//                        break;
+//                    case 'seat':
+//                        $cleaned_term = filter_var($term, FILTER_SANITIZE_NUMBER_INT);
+//                        
+//                        $fields = array('seat');
+//                        
+//                        $results = $this->search_fields($fields, $cleaned_term);
+//                        
+//                        break;
+//                }
                 
                 // pass the value to the search algo
                 
@@ -213,30 +213,84 @@ class PeopleController extends AppController {
      */
     private function determine_type($phrase) {
         
-        // TODO recognize the term pattern and return correct associated keyword
-        
         $term_arr = explode(' ', $phrase);
-    
         $output = array();
+        $types = array();
 
         foreach($term_arr as $term) {
 
-            $pattern = '#(([+]?[\(]?[0-9]{3}[\)]?)?.?([0-9]{3}.?[0-9]{4}))#';
+            $phone_pattern = '#(([+]?[\(]?[0-9]{3}[\)]?)?.?([0-9]{3}.?[0-9]{4}))#';
+            preg_match($phone_pattern, $term, $phone_matches);
 
-            preg_match($pattern, $term, $matches);
+            $seat_pattern = '#^[a-zA-Z0-9]?[-]?[0-9]{2,3}[a-zA-Z]?[^a-zA-Z0-9]?#';
+            preg_match($seat_pattern, $term, $seat_matches);
 
-            if($matches) {
-                $output[] = $matches;
-            } elseif (filter_var($term, FILTER_VALIDATE_EMAIL)) {
-                $output[] =  array('email' => $term);
+            $username_pattern = '#^[a-zA-Z]+_[a-zA-Z]+$#';
+            preg_match($username_pattern, $term, $username_matches);
+
+            $word_patter = '#^[a-zA-Z]+$#';
+            preg_match($word_patter, $term, $word_matches);
+
+            if($phone_matches) {
+                $no_symbols_phone = preg_replace('#[^0-9]#i', '', $phone_matches[0]);
+                $no_symbols_phone = strval($no_symbols_phone);
+                if(strlen($no_symbols_phone) > 7) {
+                    $formatted_phone = ''.substr($no_symbols_phone, 0, 3) . '-' . substr($no_symbols_phone, 3, 3) . '-' . substr($no_symbols_phone, 6, 4);
+                } else {
+                    $formatted_phone = ''.substr($no_symbols_phone, 0, 3) . '-' . substr($no_symbols_phone, 3, 4);
+                }
+                $output['phone'][] = $formatted_phone;
+                $types[] = 'phone';
+            } else if($seat_matches) {
+                $output['seat'] = $seat_matches;
+                $types[] = 'seat';
+            } else if($username_matches) {
+                $output['username'] = $username_matches;
+                $types[] = 'username';
+            } else if (filter_var($term, FILTER_VALIDATE_EMAIL)) {
+                $output['email'][] =  $term;
+                $types[] = 'email';
+            } else if($word_matches) {
+                $output['word'][] = $term;
+                $types[] = 'word';
             } else {
-                $output[] = array('no');
+                $output[][] = $term;
+                $types[] = 'mixed';
             }
 
         }
 
+        $output['types'] = $types;
+
         return $output;
         
+    }
+    
+    private function format_phone_query($formatted_phone_number) {
+        return '(ext LIKE \'%'.$formatted_phone_number.'%\') OR (mobile LIKE \'%'.$formatted_phone_number.'%\')';
+    }
+    
+    private function format_seat_query($seat) {
+        return '(seat LIKE \'%'.$seat.'%\')';
+    }
+    
+    private function format_username_query($username) {
+        return '(userName LIKE \'%'.$username.'%\')';
+    }
+    
+    private function format_email_query($email) {
+        return '(email LIKE \'%'.$email.'%\')';
+    }
+    
+    private function format_word_query($word) {
+        $output = '(firstName LIKE \'%'.$word.'%\') OR';
+        $output .= '(lastName LIKE \'%'.$word.'%\') OR';
+        $output .= '(title LIKE \'%'.$word.'%\') OR';
+        $output .= '(dept LIKE \'%'.$word.'%\') OR';
+        $output .= '(email LIKE \'%'.$word.'%\') OR';
+        $output .= '(seat LIKE \'%'.$word.'%\')';
+        
+        return $output;
     }
     
     /**
@@ -252,6 +306,7 @@ class PeopleController extends AppController {
         foreach($fields as $field) {
             // below is an example search (I would need to run this on all relevant fields)
             $results[] = $this->Person->query('SELECT * FROM people WHERE '.$field.' LIKE \'%'.$term.'%\' LIMIT 0, 1000');
+            // SELECT * FROM people WHERE (firstName LIKE '%mike%') OR (lastName LIKE '%byrd%') LIMIT 0, 1000
         }
         
         return $results;
