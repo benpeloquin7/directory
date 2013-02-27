168adce9e4807a512a3a71d4bd8f9d83b1581694
Mike Newell <mike@iwearshorts.com>
2013-02-26 16:47:49 -0800

    close to have the search term function complete

--- mocks/regex/regex_test.php
+++ mocks/regex/regex_test.php
@@ -25,7 +25,15 @@ $terms = array(
     'mike newell mike_newell@gspsf.com',
     'mike_newell',
     'Mike Newell mike_newell mike_newell@gspsf.com',
-    'mike newell mike_newell mike_newell@gspsf.com 3039290487'
+    'mike newell mike_newell mike_newell@gspsf.com 3039290487',
+    'mike newell mike_newell mike_newell@gspsf.com 3039290487 7_141',
+    '7-141 mike newell mike_newell mike_newell@gspsf.com 3039290487',
+    '141',
+    'C45',
+    'E45',
+    '7-141',
+    '124A',
+    '123C'
 );
 
 for($i = 0; $i < count($terms); $i++) {
@@ -43,27 +51,48 @@ function search_string($phrase) {
     $output = array();
     $types = '';
     
-    foreach($term_arr as $term) {
-        
-        $phone_pattern = '#(([+]?[\(]?[0-9]{3}[\)]?)?.?([0-9]{3}.?[0-9]{4}))#';
-        preg_match($phone_pattern, $term, $phone_matches);
-        
-        if($phone_matches) {
-            $output[] = $phone_matches;
-            $types .= ' phone ';
-        } elseif (filter_var($term, FILTER_VALIDATE_EMAIL)) {
-            $output[] =  array('email' => $term);
-            $types .= ' email ';
-        } else {
-            $output[] = array('no');
-        }
+    echo count($term_arr);
+    
+    if(count($term_arr) > 1) {
+        foreach($term_arr as $term) {
 
+            $phone_pattern = '#(([+]?[\(]?[0-9]{3}[\)]?)?.?([0-9]{3}.?[0-9]{4}))#';
+            preg_match($phone_pattern, $term, $phone_matches);
+
+            $seat_pattern = '#^[a-zA-Z0-9]?[-]?[0-9]{2,3}[a-zA-Z]?[^a-zA-Z0-9]?#';
+            preg_match($seat_pattern, $term, $seat_matches);
+
+            $username_pattern = '#^[a-zA-Z]+_[a-zA-Z]+$#';
+            preg_match($username_pattern, $term, $username_matches);
+
+            if($phone_matches) {
+                $output['phone'] = $phone_matches;
+                $types .= ' phone ';
+            } else if($seat_matches) {
+                $output['seat'] = $seat_matches;
+                $types .= ' seat ';
+            } else if($username_matches) {
+                $output['username'] = $username_matches;
+                $types .= ' username ';
+            } else if (filter_var($term, FILTER_VALIDATE_EMAIL)) {
+                $output['email'] =  $term;
+                $types .= ' email ';
+            } else {
+                $output[] = array('no');
+                $types .= ' multi-term '
+            }
+
+        }
+    } else {
+        $output['single-term'] = $term;
+        $types .= ' single-term ';
     }
+    
+        
     
     $output['types'] = $types;
     
     return $output;
-        
 };
 
 ?>
